540fe1b509527c1fc0b9a09d88a255cb83af75d2
Elio Campitelli <eliocampitelli@gmail.com>
2022-02-11 18:30:07 -0300

    Actualiza preprocesamiento a los nuevos nombres

--- preprocesamiento.R
+++ preprocesamiento.R
@@ -1,10 +1,13 @@
 datos_todos <- data.table::fread("datos/datos_perfiles.csv")
 
 
-perfiles <- unique(datos_todos[, .(perfil_id, fecha = perfil_fecha,
-                                   numero = perfil_numero,
-                                   clase = perfil_grupo_descripcion,
-                                   lon = perfil_ubicacion_longitud,
-                                   lat = perfil_ubicacion_latitud)])
+perfiles <- unique(datos_todos[, .(perfil_id,
+                                   fecha = s_fecha,
+                                   numero = s_perfil_no,
+                                   clase = s_grupo,
+                                   lon = s_lon,
+                                   lat = s_lat)])
 
 data.table::fwrite(perfiles, "datos/perfiles.csv")
+
+
